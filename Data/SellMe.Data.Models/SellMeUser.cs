5ad1b78810a8a1fb48be987c885ef0bd1dab138f
Kristian <kriskobota@abv.bg>
2019-06-20 11:23:16 +0300

    Initial commit

---
+++ Data/SellMe.Data.Models/SellMeUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SellMe.Data.Models
+{
+    // Add profile data for application users by adding properties to the SellMeUser class
+    public class SellMeUser : IdentityUser
+    {
+    }
+}
---
+++ Data/SellMe.Data/SellMeDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SellMe.Data.Models;
+
+namespace SellMe.Data
+{
+    public class SellMeDbContext : IdentityDbContext<SellMeUser>
+    {
+        public SellMeDbContext(DbContextOptions<SellMeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SellMeUser> SellMeUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
