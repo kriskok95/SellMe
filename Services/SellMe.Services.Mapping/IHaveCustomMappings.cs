24a66115825e51abcdc7d8e52dc22009dcdff0dd
Kristian <kriskobota@abv.bg>
2019-06-26 00:40:13 +0300

    Installed and configured AutoMapper from Nikolay.IT ASP.Net Core MVC Template

---
+++ Services/SellMe.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace SellMe.Services.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configuration);
+    }
+}
---
+++ Services/SellMe.Services.Mapping/IMapFrom.cs
@@ -0,0 +1,7 @@
+﻿namespace SellMe.Services.Mapping
+{
+    // ReSharper disable once UnusedTypeParameter
+    public interface IMapFrom<T>
+    {
+    }
+}
