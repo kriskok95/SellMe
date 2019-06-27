3fea4303420528a2a7d826760ea93bf7bdf00648
Kristian <kriskobota@abv.bg>
2019-06-27 15:16:32 +0300

    Introduced DbContextFactory

---
+++ Data/SellMe.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+namespace SellMe.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<SellMeDbContext>
+    {
+        public SellMeDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), $"../../Web/SellMe.Web"))
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<SellMeDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            // Stop client query evaluation
+            builder.ConfigureWarnings(w => w.Throw(RelationalEventId.QueryClientEvaluationWarning));
+
+            return new SellMeDbContext(builder.Options);
+        }
+    }
+}
