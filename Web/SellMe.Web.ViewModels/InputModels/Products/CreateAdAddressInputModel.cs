aaa55b1abec17fac32dc5e957e6f2fccd615e064
Kristian <kriskobota@abv.bg>
2019-06-28 21:50:44 +0300

    Added validations for CreateAdAddressInputModel

--- Web/SellMe.Web.ViewModels/InputModels/Products/CreateAdAddressInputModel.cs
+++ Web/SellMe.Web.ViewModels/InputModels/Products/CreateAdAddressInputModel.cs
@@ -2,21 +2,31 @@
 {
     using SellMe.Data.Models;
     using SellMe.Services.Mapping;
+    using System.ComponentModel.DataAnnotations;
 
     public class CreateAdAddressInputModel : IMapTo<Address>
     {
+        [Required]
         public string Country { get; set; }
 
+        [Required]
         public string City { get; set; }
 
+        [Required]
         public string District { get; set; }
 
+        [Required]
         public string  Street { get; set; }
 
+        [Required]
+        [DataType(DataType.PostalCode)]
         public int ZipCode { get; set; }
 
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
     }
 }
--- Web/SellMe.Web.ViewModels/InputModels/Products/CreateAdDetailInputModel.cs
+++ Web/SellMe.Web.ViewModels/InputModels/Products/CreateAdDetailInputModel.cs
@@ -6,25 +6,32 @@
 
     public class CreateAdDetailInputModel
     {
-        private const string PriceErrorMessage = "The price can't be a negative number!";
-        private const string AvailabilityErrorMessage = "The availability can't be a negative number!";
+        private const string PriceErrorMessage = "{0} can't be a negative number!";
+        private const string AvailabilityErrorMessage = "{0} can't be a negative number!";
+        private const int TitleMinLength = 5;
+        private const int TitleMaxLength = 120;
+        private const string TitleLengthErrorMessage = "{0} should be between {2} and {1} characters length!";
+        private const int DescriptionMinLength = 20;
+        private const string PriceMinValue = "0.1";
+        private const string PriceMaxValue = "79228162514264337593543950335";
+        private const int AvailabilityMinValue = 1;
+        private const int AvailabilityMaxValue = int.MaxValue;
 
         public CreateAdDetailInputModel()
         {
             this.Images = new List<IFormFile>();
         }
 
-        [MinLength(10)]
-        [MaxLength(120)]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength, ErrorMessage = TitleLengthErrorMessage)]
         [Required]
         public string Title { get; set; }
 
-        [MinLength(20)]
+        [MinLength(DescriptionMinLength)]
         [Required]
         public string Description { get; set; }
 
         [Required]
-        [Range(typeof(decimal), "0.1", "79228162514264337593543950335", ErrorMessage = PriceErrorMessage)]
+        [Range(typeof(decimal), PriceMinValue, PriceMaxValue, ErrorMessage = PriceErrorMessage)]
         public decimal Price { get; set; }
 
         [Required]
@@ -33,13 +40,14 @@
         [Required]
         public string SubCategory { get; set; }
 
-        [Range(typeof(int), "1", "2147483647", ErrorMessage = AvailabilityErrorMessage)]
+        [Range(AvailabilityMinValue, AvailabilityMaxValue, ErrorMessage = AvailabilityErrorMessage)]
         [Required]
         public int Availability { get; set; }
 
         [Required]
         public string Condition { get; set; }
 
+        //TODO: Add validations for image types
         [DataType(DataType.Upload)]
         public ICollection<IFormFile> Images { get; set; }
     }
