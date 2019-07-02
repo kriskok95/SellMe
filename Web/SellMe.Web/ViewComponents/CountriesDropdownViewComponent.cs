1bc2de1556461623eeab0e9d7527712761e3ca63
Kristian <kriskobota@abv.bg>
2019-07-02 17:32:30 +0300

    Created CountriesDropdown view component

---
+++ Web/SellMe.Web/ViewComponents/CountriesDropdownViewComponent.cs
@@ -0,0 +1,21 @@
+﻿namespace SellMe.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using SellMe.Services.Interfaces;
+
+    public class CountriesDropdownViewComponent : ViewComponent
+    {
+        private readonly IAddressService addressService;
+
+        public CountriesDropdownViewComponent(IAddressService addressService)
+        {
+            this.addressService = addressService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var countries = this.addressService.GetAllCountries();
+            return this.View(countries);
+        }
+    }
+}
