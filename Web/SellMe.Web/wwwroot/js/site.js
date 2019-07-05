085de6ee1d7be0f9f6266cd822ebf20503fb709f
Kristian <kriskobota@abv.bg>
2019-07-05 21:15:46 +0300

    Changed upload images design.

--- Web/SellMe.Web/wwwroot/js/site.js
+++ Web/SellMe.Web/wwwroot/js/site.js
@@ -66,3 +66,9 @@ $(document).ready(function () {
 		});
 	});
 });
+
+//Input file 
+$(".custom-file-input").on("change", function () {
+	var fileName = $(this).val().split("\\").pop();
+	$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
