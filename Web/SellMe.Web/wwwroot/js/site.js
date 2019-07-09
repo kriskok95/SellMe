a974023a5c8bdbd0d2c5bb86ba4db7589ea76bf4
Kristian <kriskobota@abv.bg>
2019-07-09 17:53:40 +0300

    Introduced "MyFavorites" view.

--- Web/SellMe.Web/wwwroot/js/site.js
+++ Web/SellMe.Web/wwwroot/js/site.js
@@ -72,3 +72,49 @@ $(".custom-file-input").on("change", function () {
 	var fileName = $(this).val().split("\\").pop();
 	$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 });
+
+
+//Add to favorites
+$(document).ready(function () {
+	$(".container-fluid").on('click', '.addToFavorites', function () {
+		event.stopImmediatePropagation();
+		var adId = $(this).val();
+
+		$.ajax({
+			url: "/Favorites/Add",
+			type: "GET",
+			data: "adId=" + adId,
+			dataType: 'json',
+			success: function (response) {
+				alert("You have successfully added this ad to favorites!");
+				$("#" + adId).replaceWith('<button type="button" value="' + adId + '"' + 'class="btn btn-danger btn-block mt-4 removeFromFavorites" id="' + adId + '">Remove From Favorites</button>');
+			}
+		});
+    });
+
+    //Remove from favorites
+	$(".container-fluid").on('click', '.removeFromFavorites', function
+		() {
+			event.stopImmediatePropagation();
+			var adId = $(this).val();
+
+			$.ajax({
+				url: "/Favorites/Remove",
+				type: "GET",
+				data: "adId=" + adId,
+				dataType: 'json',
+				success: function (response) {
+					alert("You have successfully removed this ad from favorites!");
+					$("#" + adId).replaceWith('<button type="button" value="' + adId + '"' + 'class="btn btn-primary btn-block mt-4 addToFavorites" id="' + adId + '">Add To Favorites</button>');
+				}
+			});
+		});
+});
+
+$(document).ready(function () {
+	$(".removeAdFromDom").click(function () {
+		let adId = $(this).val();
+		console.log(adId);
+		$(this).remove();
+	});
+});
