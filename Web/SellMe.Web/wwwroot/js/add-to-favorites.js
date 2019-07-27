e5c2d7fe913ccbe265a0b363b7c41f53dc8568d9
Kristian <kriskobota@abv.bg>
2019-07-27 23:23:48 +0300

    Added favorites button and removed _LayoutViewComponent.

---
+++ Web/SellMe.Web/wwwroot/js/add-to-favorites.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $(".container-fluid").on('click', '.addToFavorites', function test () {
+        event.stopImmediatePropagation();
+        var adId = $(this).val();
+        console.log(adId);
+
+        $.ajax({
+            url: "/Favorites/Add",
+            type: "GET",
+            data: "adId=" + adId,
+            dataType: 'json',
+            success: function (response) {
+                alert("You have successfully added this ad to favorites!");
+                $("#" + adId).replaceWith('<button value="' + adId + '"' + 'class="btn removeFromFavorites" id="' + adId + '"><img src="/img/favorite.png" alt="favorite" /></button>');
+            }
+        });
+    });
+
+    $(".container-fluid").on('click', '.removeFromFavorites', function
+        () {
+            event.stopImmediatePropagation();
+            var adId = $(this).val();
+
+            $.ajax({
+                url: "/Favorites/Remove",
+                type: "GET",
+                data: "adId=" + adId,
+                dataType: 'json',
+                success: function (response) {
+                    alert("You have successfully removed this ad from favorites!");
+                    $("#" + adId).replaceWith('<button value="' + adId + '"' + 'class="btn addToFavorites" id="' + adId + '"><img src="/img/not-favorite.png" alt="not-favorite" /></button>');
+                }
+            });
+        });
+});
--- Web/SellMe.Web/wwwroot/js/site.js
+++ Web/SellMe.Web/wwwroot/js/site.js
@@ -50,40 +50,4 @@ $(".custom-file-input").on("change", function () {
 });
 
 
-//Add to favorites
-$(document).ready(function () {
-	$(".container-fluid").on('click', '.addToFavorites', function () {
-		event.stopImmediatePropagation();
-		var adId = $(this).val();
-
-		$.ajax({
-			url: "/Favorites/Add",
-			type: "GET",
-			data: "adId=" + adId,
-			dataType: 'json',
-			success: function (response) {
-				alert("You have successfully added this ad to favorites!");
-				$("#" + adId).replaceWith('<button type="button" value="' + adId + '"' + 'class="btn btn-danger btn-block mt-4 removeFromFavorites" id="' + adId + '">Remove From Favorites</button>');
-			}
-		});
-    });
-
-    //Remove from favorites
-	$(".container-fluid").on('click', '.removeFromFavorites', function
-		() {
-			event.stopImmediatePropagation();
-			var adId = $(this).val();
-
-			$.ajax({
-				url: "/Favorites/Remove",
-				type: "GET",
-				data: "adId=" + adId,
-				dataType: 'json',
-				success: function (response) {
-					alert("You have successfully removed this ad from favorites!");
-					$("#" + adId).replaceWith('<button type="button" value="' + adId + '"' + 'class="btn btn-primary btn-block mt-4 addToFavorites" id="' + adId + '">Add To Favorites</button>');
-				}
-			});
-		});
-});
 
