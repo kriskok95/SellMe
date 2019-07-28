878dd7ed0acaee31c239d961c804c534eeadfa8f
Kristian <kriskobota@abv.bg>
2019-07-28 11:42:33 +0300

    Added favorites button at the index page.

---
+++ Web/SellMe.Web/wwwroot/js/add-to-favorites-index.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function () {
+    $(".container-fluid").on('click', '.addToFavorites', function test() {
+        event.stopImmediatePropagation();
+        var adId = $(this).val();
+        console.log(adId);
+
+        $.ajax({
+            url: "/Favorites/Add",
+            type: "GET",
+            data: "adId=" + adId,
+            dataType: 'json',
+            success: function (response) {
+                $(function () {
+                    $('#add-favorites-modal').modal('show');
+                    $(function () {
+                        var myModal = $('#add-favorites-modal');
+                        clearTimeout(myModal.data('hideInterval'));
+                        myModal.data('hideInterval', setTimeout(function () {
+                            myModal.modal('hide');
+                        }, 2500));
+                    });
+                });
+                $("#" + adId).replaceWith('<button value="' + adId + '"' + 'class="btn removeFromFavorites" id="' + adId + '"><img src="/img/favorite-index.png" alt="favorite" /></button>');
+            }
+        });
+    });
+
+    $(".container-fluid").on('click', '.removeFromFavorites', function
+        () {
+        event.stopImmediatePropagation();
+        var adId = $(this).val();
+
+        $.ajax({
+            url: "/Favorites/Remove",
+            type: "GET",
+            data: "adId=" + adId,
+            dataType: 'json',
+            success: function (response) {
+                $(function () {
+                    $('#remove-favorites-modal').modal('show');
+                    $(function () {
+                        var myModal = $('#remove-favorites-modal');
+                        clearTimeout(myModal.data('hideInterval'));
+                        myModal.data('hideInterval', setTimeout(function () {
+                            myModal.modal('hide');
+                        }, 2500));
+                    });
+                });
+                $("#" + adId).replaceWith('<button value="' + adId + '"' + 'class="btn addToFavorites" id="' + adId + '"><img src="/img/not-favorite-index.png" alt="not-favorite" /></button>');
+            }
+        });
+    });
+});
