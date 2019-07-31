8344c0199f67609505aafed104fe9686c68b4be7
Kristian <kriskobota@abv.bg>
2019-07-31 10:25:42 +0300

    Added validations for creating a review.

--- Web/SellMe.Web.ViewModels/InputModels/Reviews/ReviewInputModel.cs
+++ Web/SellMe.Web.ViewModels/InputModels/Reviews/ReviewInputModel.cs
@@ -1,13 +1,24 @@
 ﻿namespace SellMe.Web.ViewModels.InputModels.Reviews
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ReviewInputModel
     {
+        public const int ReviewMinimumLength = 5;
+        public const string ReviewMinimumLengthErrorMessage = "The review should be at least {1} characters.";
+
+        [Required]
         public string OwnerId { get; set; }
 
+        [Required]
         public string CreatorId { get; set; }
 
+        [Required]
+        [MinLength(ReviewMinimumLength, ErrorMessage = ReviewMinimumLengthErrorMessage)]
         public string Content { get; set; }
 
+        //TODO: Create custom validation attribute for rating field.
+        [Required]
         public int Rating { get; set; }
     }
 }
