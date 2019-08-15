c99c7e73516f4f15d5ea49e750efe7ba898913a7
Kristian <kriskobota@abv.bg>
2019-08-15 13:01:55 +0300

    Introduced administration area.

---
+++ Web/SellMe.Web/Areas/Administration/Controllers/AdministrationsController.cs
@@ -0,0 +1,16 @@
+﻿namespace SellMe.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+    using SellMe.Common;
+
+    public class AdministrationsController : Controller
+    {
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        [Area("Administration")]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Web/SellMe.Web/Areas/Administration/Controllers/AdsController.cs
@@ -0,0 +1,26 @@
+﻿namespace SellMe.Web.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using SellMe.Common;
+    using Microsoft.AspNetCore.Mvc;
+    using SellMe.Services.Interfaces;
+
+    public class AdsController : Controller
+    {
+        private readonly IAdsService adsService;
+
+        public AdsController(IAdsService adsService)
+        {
+            this.adsService = adsService;
+        }
+
+        //[Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        //[Area("Administration")]
+        //public IActionResult Approvement()
+        //{
+        //    var adsForApprovementViewModels = this.adsService.GetAdsForApprovementViewModels();
+
+
+        //}
+    }
+}
